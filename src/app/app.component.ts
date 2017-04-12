7a30cad6c298f75f4ea547fbf8e614fdb077f6ab
angularist <gendash@gmail.com>
2017-04-13 00:52:20 +0300

    Uploading directory after demo of the problem is complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataStoreService} from "./data-store.service";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h3>Here are some names of Disney's characters:</h3>
+    <ul>
+      <li *ngFor="let name of dataStore.getNames() | slice:0:2">{{name}}</li>
+      <!--<li *ngFor="let name of dataStore.getNames()">{{name}}</li>-->
+    </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    console.log(this.dataStore.getNames());
+  }
+
+  private dataStore: DataStoreService;
+
+  constructor(dataStore: DataStoreService) {
+    this.dataStore = dataStore;
+  }
 }
